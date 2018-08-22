8b50385dcb64f3a765daf7cc7cea30eb2dcd3a12
BunnygotFangs <kimberly@thehilts.com>
2018-08-22 15:40:20 -0500

    update footerwith addtional links

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -8,22 +8,37 @@ const Footer = props => (
   <div>
 
             <footer id="footer">
+<section>
+  <ul className="icons">
+    <li><a href="https://www.mvplogistics.com/solutions/small-package-shipping/" className="icon style2 fa-gift"><span className="label">Facebook</span></a></li>
+    <li><a href="https://www.mvplogistics.com/solutions/freight/" className="icon style2 fa-truck"><span className="label">Instagram</span></a></li>
+    <li><a href="https://www.mvplogistics.com/solutions/international-ocean-air/" className="icon style2 fa-globe"><span className="label">LinkedIn</span></a></li>
+    <li><a href="https://www.mvplogistics.com/solutions/integration-visibility/" className="icon style2 fa-laptop"><span className="label">Phone</span></a></li>
+    <li><a href="https://www.mvplogistics.com/solutions/distribution-and-fulfillment/" className="icon style2 fa-opencart"><span className="label">Email</span></a></li>
+    <li><a href="https://www.mvplogistics.com/solutions/projects/" className="icon style2 fa-tasks"><span className="label">Facebook</span></a></li>
+    <li><a href="https://www.mvplogistics.com/solutions/onsite-management/" className="icon style2 fa-clipboard"><span className="label">Instagram</span></a></li>
+    <li><a href="https://www.mvplogistics.com/solutions/minneapolis-area-priority-service/" className="icon style2 fa-industry"><span className="label">LinkedIn</span></a></li>
+  </ul>
+
+<br></br>
+</section>
               <div className="inner">
-    
                 <section>
-                  <h2>Follow</h2>
+                  <h2>Follow Us</h2>
                   <ul className="icons">
-                    <li><a href="https://www.facebook.com/mvpship" className="icon style2 fa-facebook"><span className="label">Facebook</span></a></li>
-                    <li><a href="https://www.instagram.com/mvp_logistics/" className="icon style2 fa-instagram"><span className="label">Instagram</span></a></li>
-                    <li><a href="https://www.linkedin.com/company/mvpship/" className="icon style2 fa-linkedin"><span className="label">LinkedIn</span></a></li>
-                    <li><a href="tel:18883905320" className="icon style2 fa-phone"><span className="label">Phone</span></a></li>
-                    <li><a href="mailto:info@mvpship.com" className="icon style2 fa-envelope-o"><span className="label">Email</span></a></li>
+                    <li><a href="https://www.facebook.com/mvpship" className="icon style1 fa-facebook"><span className="label">Facebook</span></a></li>
+                    <li><a href="https://www.instagram.com/mvp_logistics/" className="icon style1 fa-instagram"><span className="label">Instagram</span></a></li>
+                    <li><a href="https://www.linkedin.com/company/mvpship/" className="icon style1 fa-linkedin"><span className="label">LinkedIn</span></a></li>
+                    <li><a href="tel:18883905320" className="icon style1 fa-phone"><span className="label">Phone</span></a></li>
+                    <li><a href="mailto:info@mvpship.com" className="icon style1 fa-envelope-o"><span className="label">Email</span></a></li>
                   </ul>
                 </section>
-                <ul className="copyright">
-                  <li>&copy; MVP Logistics and Services. All rights reserved 2018</li>
-                </ul>
+
               </div>
+              <br></br>
+
+                  <ul>&copy; MVP Logistics and Services. All rights reserved 2018</ul>
+
             </footer>
   
         </div>
