fa1dbd1a7eccf014f735ff18cf7df1b9b280d71f
Joe Booth <joseph.booth3@gmail.com>
2018-08-24 21:04:50 -0500

    adding link to admin

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -34,6 +34,9 @@ class Nav extends Component {
           <li className="nav-item active">
             <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
           </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/admin">Admin</a>
+          </li>
           
         </ul>
         <span className="navbar-text">
